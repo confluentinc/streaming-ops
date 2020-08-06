6d7c27ded9c75502b6f8b9642f8a19fa5a1f442f
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-08-05 19:20:55 -0500

    DEVX-1985: WIP connector controller

--- images/connect-controller/start.sh
+++ images/connect-controller/start.sh
@@ -3,17 +3,7 @@
 : "${CONNECTORS_PATH?Need to set CONNECTORS_PATH}"
 : "${CONFIG_FILE_PATH?Need to set CONFIG_FILE_PATH}"
 
-# TODO: 
-# * Populate variables from /etc/config/kafka/kafka.properties
-# * template from each connector file into a temp file
-# * Either
-# * just curl post/put if connect can do this idempotently
-#   OR
-# * Check for existence of connector,
-#     then either POST to /connector
-#     or PUT to /connectors/<name>/config
-
-URL=${URL:-http://connect/connectors}
+BASE_URL=${BASE_URL:-http://connect}
 JQ_ARGS_FROM_CONFIG_FILE=$(
 while read line
 do
@@ -27,7 +17,19 @@ done < $CONFIG_FILE_PATH)
 for FILE in $CONNECTORS_PATH;
 do
   JQ="jq -c -n $JQ_ARGS_FROM_CONFIG_FILE -f $FILE"
+
   CONNECTOR_CONFIG=$(eval $JQ)
-  curl -s -i -X POST -H "Content-Type: application/json" --data "$CONNECTOR_CONFIG" $URL 
+  CONNECTOR_NAME=$(echo $CONNECTOR_CONFIG | jq -r '.name')
+
+  CONNECTOR_EXISTS_RESULT=$(curl -s -o /dev/null -I -w "%{http_code}" -XGET -H "Accpet: application/json" "$BASE_URL/connectors/$CONNECTOR_NAME")
+
+  [[ "$CONNECTOR_EXISTS_RESULT" == "200" ]] && {
+    echo "updating existing connector config: $CONNECTOR_NAME"
+    curl -s -i -X PUT -H "Content-Type: application/json" --data "$CONNECTOR_CONFIG" "$BASE_URL/connectors/$CONNECTOR_NAME/config"
+  } || {
+    echo "creating new connector: $CONNECTOR_NAME"
+    curl -s -i -X POST -H "Content-Type: application/json" --data "$CONNECTOR_CONFIG" "$BASE_URL/connectors"
+  }
+
 done
 
