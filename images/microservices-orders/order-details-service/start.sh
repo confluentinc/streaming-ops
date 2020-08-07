ec361dfe8a4a543837440d7ae761a7a24170ad48
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-08-07 13:36:23 -0500

    DEVX-1983: add order-details-service

---
+++ images/microservices-orders/order-details-service/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONFIG_FILE=${CONFIG_FILE:-/etc/config/kafka/kafka.properties}
+
+BOOTSTRAP_SERVERS=$(grep "bootstrap.servers" $CONFIG_FILE | cut -d= -f2)
+SCHEMA_REGISTRY_URL=$(grep "schema.registry.url" $CONFIG_FILE | cut -d= -f2)
+RESTPORT=${RESTPORT:-18894}
+JAR=${JAR:-"/usr/share/java/kafka-streams-examples/kafka-streams-examples-5.5.1-standalone.jar"}
+CONFIG_FILE_ARG="--config-file $CONFIG_FILE"
+ADDITIONAL_ARGS=${ADDITIONAL_ARGS:-""}
+
+echo "starting orders-service"
+env
+
+java -cp $JAR io.confluent.examples.streams.microservices.OrderDetailsService --bootstrap-servers $BOOTSTRAP_SERVERS --schema-registry $SCHEMA_REGISTRY_URL $CONFIG_FILE_ARG $ADDITIONAL_ARGS
+
