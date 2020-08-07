c9c15af3ef78ac1ca529941d0616e19429e76a3c
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-08-07 09:40:23 -0500

    DEVX-1983: adding connect-controller build action

--- images/connect-controller/start.sh
+++ images/connect-controller/start.sh
@@ -16,17 +16,33 @@ done < $CONFIG_FILE_PATH)
 
 for FILE in $CONNECTORS_PATH;
 do
+	echo ""
+	echo "========================================"
+
   JQ="jq -c -n $JQ_ARGS_FROM_CONFIG_FILE -f $FILE"
 
   CONNECTOR_CONFIG=$(eval $JQ)
   CONNECTOR_NAME=$(echo $CONNECTOR_CONFIG | jq -r '.name')
 
+	echo "Evaluating connector: $CONNECTOR_NAME"
+
   CONNECTOR_EXISTS_RESULT=$(curl -s -o /dev/null -I -w "%{http_code}" -XGET -H "Accpet: application/json" "$BASE_URL/connectors/$CONNECTOR_NAME")
 
   [[ "$CONNECTOR_EXISTS_RESULT" == "200" ]] && {
-    echo "updating existing connector config: $CONNECTOR_NAME"
-    CONNECTOR_CONFIG_CONFIG=$(echo "$CONNECTOR_CONFIG" | jq -r -c '.config')
-    curl -s -i -X PUT -H "Content-Type: application/json" --data "$CONNECTOR_CONFIG_CONFIG" "$BASE_URL/connectors/$CONNECTOR_NAME/config"
+   	 
+    DESIRED_CONNECTOR_CONFIG=$(echo "$CONNECTOR_CONFIG" | jq -r -c '.config')
+		echo $DESIRED_CONNECTOR_CONFIG | jq -S .
+
+		echo "vvvvvvvvvvvvvvvvvvvvvvv"
+		CURRENT_CONNECTOR_CONFIG=$(curl -s -X GET -H "Content-Type: application/json" "$BASE_URL/connectors/$CONNECTOR_NAME/config")
+		echo $CURRENT_CONNECTOR_CONFIG | jq -S .
+
+		if diff <(echo $DESIRED_CONNECTOR_CONFIG | jq -S .) <(echo $CURRENT_CONNECTOR_CONFIG | jq -S .); then
+			echo "No config changes for $CONNECTOR_NAME"
+		else		
+			echo "Updating existing connector config: $CONNECTOR_NAME"
+    	curl -s -i -X PUT -H "Content-Type: application/json" --data "$DESIRED_CONNECTOR_CONFIG" "$BASE_URL/connectors/$CONNECTOR_NAME/config"
+		fi
   } || {
     echo "creating new connector: $CONNECTOR_NAME"
     curl -s -i -X POST -H "Content-Type: application/json" --data "$CONNECTOR_CONFIG" "$BASE_URL/connectors"
