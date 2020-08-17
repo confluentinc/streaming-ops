d0f7df53b5205dd4a4365874219c71a34f628d4c
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-08-17 16:50:44 -0500

    DEVX-1984: WIP ccloud operator

--- images/ccloud-operator/hooks/operator.sh
+++ images/ccloud-operator/hooks/operator.sh
@@ -17,6 +17,28 @@ kubernetes:
   jqFilter: ".data"
 EOF
 else
-  cat $BINDING_CONTEXT_PATH
+  ARRAY_COUNT=`jq -r '. | length-1' $BINDING_CONTEXT_PATH`
+  echo "got $ARRAY_COUNT items in binding, looking at first"
+
+  TYPE=$(jq -r .[0].type $BINDING_CONTEXT_PATH)
+  EVENT=$(jq -r .[0].watchEvent $BINDING_CONTEXT_PATH)
+
+  if [[ "$TYPE" == "Synchronization" ]]; then
+    #KEYS=$(jq -c -r '.[0].objects | .[].object.data | keys | .[]' $BINDING_CONTEXT_PATH)
+		#for KEY in $KEYS; do
+		#	CONFIG=$(jq -c -r ".[].objects | .[].object.data | select(has(\"$KEY\")) | .\"$KEY\"" $BINDING_CONTEXT_PATH)
+		#done	
+    echo "Got synchronization event"
+    exit 0
+  elif [[ "$TYPE" == "Event" ]]; then
+    DATA=$(jq -r '.[0].object.data' $BINDING_CONTEXT_PATH)
+    KEY=$(echo $DATA | jq -r -c 'keys | .[0]')
+    CONFIG=$(echo $DATA | jq -r -c ".\"$KEY\"")
+    if [[ "$EVENT" == "Deleted" ]]; then
+      echo "delete"
+    else
+      echo "apply"
+    fi
+  fi
 fi
 
