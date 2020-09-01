82a71c9934d69e32cafd2785b989a88a1d0b2faf
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-09-01 08:43:17 -0500

    DEVX-1984: Updating microservices for ccloud-operator

--- images/microservices-orders/orders-and-payments-simulator/start.sh
+++ images/microservices-orders/orders-and-payments-simulator/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CONFIG_FILE=${CONFIG_FILE:-/etc/config/kafka/kafka.properties}
+STARTUP_DELAY=${STARTUP_DELAY:-0}
+
+for f in /etc/config/orders-and-payments-simulator/*.properties; do (cat "${f}"; echo) >> /etc/config/orders-and-payments-simulator/orders-and-payments-simulator.properties; done
+
+CONFIG_FILE=${CONFIG_FILE:-/etc/config/orders-and-payments-simulator/orders-and-payments-simulator.properties}
 
 BOOTSTRAP_SERVERS=$(grep "bootstrap.servers" $CONFIG_FILE | cut -d= -f2)
 SCHEMA_REGISTRY_URL=$(grep "schema.registry.url" $CONFIG_FILE | cut -d= -f2)
@@ -11,6 +15,8 @@ ADDITIONAL_ARGS=${ADDITIONAL_ARGS:-""}
 
 echo "starting orders-and-payments-simulator"
 env
+
+sleep $STARTUP_DELAY
 
 java -cp $JAR io.confluent.examples.streams.microservices.PostOrdersAndPayments --bootstrap-servers $BOOTSTRAP_SERVERS --schema-registry $SCHEMA_REGISTRY_URL --order-service-url "http://orders-service" $CONFIG_FILE_ARG
 
