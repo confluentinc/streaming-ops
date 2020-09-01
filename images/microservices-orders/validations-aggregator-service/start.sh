5c608b0d8d7e62372fb21b9679bcdb9c72bac2de
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-09-01 08:09:42 -0500

    DEVX-1984: Updating microservices for ccloud-operator

--- images/microservices-orders/validations-aggregator-service/start.sh
+++ images/microservices-orders/validations-aggregator-service/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-CONFIG_FILE=${CONFIG_FILE:-/etc/config/kafka/kafka.properties}
+STARTUP_DELAY=${STARTUP_DELAY:-0}
+
+for f in /etc/config/validations-aggregator-service/*.properties; do (cat "${f}"; echo) >> /etc/config/validations-aggregator-service/validations-aggregator-service.properties; done
+
+CONFIG_FILE=${CONFIG_FILE:-/etc/config/validations-aggregator/validations-aggregator-service.properties}
 
 BOOTSTRAP_SERVERS=$(grep "bootstrap.servers" $CONFIG_FILE | cut -d= -f2)
 SCHEMA_REGISTRY_URL=$(grep "schema.registry.url" $CONFIG_FILE | cut -d= -f2)
@@ -11,6 +15,8 @@ ADDITIONAL_ARGS=${ADDITIONAL_ARGS:-""}
 
 echo "starting validations-aggregator-service"
 env
+
+sleep $STARTUP_DELAY
 
 java -cp $JAR io.confluent.examples.streams.microservices.ValidationsAggregatorService --bootstrap-servers $BOOTSTRAP_SERVERS --schema-registry $SCHEMA_REGISTRY_URL $CONFIG_FILE_ARG $ADDITIONAL_ARGS
 
