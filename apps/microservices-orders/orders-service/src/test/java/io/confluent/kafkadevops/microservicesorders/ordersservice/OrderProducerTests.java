d438a2ed9793480c764e70556bb375cb52af9fc8
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-10-09 15:06:18 -0500

    DEVX-2143: WIP orders-service in springboot

--- apps/microservices-orders/orders-service/src/test/java/io/confluent/kafkadevops/microservicesorders/ordersservice/OrderProducerTests.java
+++ apps/microservices-orders/orders-service/src/test/java/io/confluent/kafkadevops/microservicesorders/ordersservice/OrderProducerTests.java
@@ -1,39 +1,71 @@
 package io.confluent.kafkadevops.microservicesorders.ordersservice;
 
+import org.apache.kafka.clients.consumer.Consumer;
 import io.confluent.examples.streams.avro.microservices.Order;
 import io.confluent.examples.streams.avro.microservices.OrderState;
 import io.confluent.examples.streams.avro.microservices.Product;
-import org.junit.ClassRule;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.awaitility.Awaitility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.test.rule.EmbeddedKafkaRule;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EmbeddedKafka
 public class OrderProducerTests {
 
-  @Value("${orders-topic.name}")
-  private static String topic;
+  private Logger logger = LoggerFactory.getLogger(OrderProducerTests.class);
+
+  @Autowired
+  private KafkaProperties properties;
 
   @Autowired
   private OrderProducer producer;
 
-  @ClassRule
-  public static EmbeddedKafkaRule kafka = new EmbeddedKafkaRule(
-    1, true, "orders-test");
-
   @Test
-  public void testSend() {
-    producer.produceOrder(
-      new Order("123", 123L, OrderState.CREATED,
-        Product.UNDERPANTS, 1, 8.34));
-    assertThat(true);
-  }
+  public void testSend() throws InterruptedException {
+    Order o1 = new Order("123", 123L, OrderState.CREATED, Product.UNDERPANTS, 1, 8.34);
 
+    List<Order> producedOrders = List.of(o1);
+    producedOrders.forEach(producer::produceOrder);
+
+    Consumer<String, Order> consumer = new DefaultKafkaConsumerFactory<String, Order>(
+      properties.buildConsumerProperties()).createConsumer();
+    consumer.subscribe(Collections.singleton("orders"));
+
+    List<Order> actual = new CopyOnWriteArrayList<>();
+    ExecutorService service = Executors.newSingleThreadExecutor();
+    Future<?> consumingTask = service.submit(() -> {
+      while (!Thread.currentThread().isInterrupted()) {
+        ConsumerRecords<String, Order> records = KafkaTestUtils.getRecords(consumer, 100);
+        for (ConsumerRecord<String, Order> rec : records) {
+          actual.add(rec.value());
+        }
+      }
+    });
+
+    try {
+      Awaitility.await().atMost(5, TimeUnit.MINUTES)
+        .until(() -> actual.equals(producedOrders));
+    } finally {
+      consumingTask.cancel(true);
+      service.awaitTermination(100, TimeUnit.MILLISECONDS);
+    }
+  }
 }
