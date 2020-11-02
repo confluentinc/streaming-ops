6f0ce95101396d6522487ddce310cc5e4e5501c5
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-11-02 12:55:19 -0600

    DEVX-2143: functioning unit test for orders-service'

--- apps/microservices-orders/orders-service/src/test/java/io/confluent/kafkadevops/microservicesorders/ordersservice/OrderProducerTests.java
+++ apps/microservices-orders/orders-service/src/test/java/io/confluent/kafkadevops/microservicesorders/ordersservice/OrderProducerTests.java
@@ -12,12 +12,14 @@ import org.awaitility.Awaitility;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
+
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.test.EmbeddedKafkaBroker;
@@ -45,7 +47,7 @@ public class OrderProducerTests {
   private EmbeddedKafkaBroker testBroker;
 
   @Test
-  public void testSend() throws InterruptedException {
+  public void testSend() throws Exception {
     Order o1 = new Order("123", 123L, OrderState.CREATED, Product.UNDERPANTS, 1, 8.34);
     Order o2 = new Order("987", 987L, OrderState.CREATED, Product.JUMPERS, 2, 5.12);
 
@@ -58,35 +60,41 @@ public class OrderProducerTests {
     configs.put("schema.registry.url", "mock://orders-service");
     configs.put("key.deserializer",StringDeserializer.class.getName());
     configs.put("value.deserializer",KafkaAvroDeserializer.class.getName());
+    configs.put("specific.avro.reader", "true");
 
     Consumer<String, Order> consumer = new DefaultKafkaConsumerFactory<String, Order>(configs).createConsumer();
     consumer.subscribe(Collections.singleton("orders"));
 
-    List<Order> actual = new CopyOnWriteArrayList<>();
+
     ExecutorService service = Executors.newSingleThreadExecutor();
-    Future<?> consumingTask = service.submit(() -> {
-      while (!Thread.currentThread().isInterrupted()) {
+    Future<List<Order>> consumingTask = service.submit(() -> {
+      List<Order> actual = new CopyOnWriteArrayList<>();
+      while (actual.size() < producedOrders.size() && Thread.currentThread().isInterrupted() == false) {
         ConsumerRecords<String, Order> records = KafkaTestUtils.getRecords(consumer, 100);
         for (ConsumerRecord<String, Order> rec : records) {
           actual.add(rec.value());
         }
       }
+      return actual;
     });
 
     try {
-      Awaitility.await().atMost(5, TimeUnit.MINUTES)
-        .until(() -> consumingTask.isDone());
-    } finally {
-      consumingTask.cancel(true);
-      service.awaitTermination(100, TimeUnit.MILLISECONDS);
-    }
 
-    try {
-      // The Future returns void, in case there was an exception in processing we can surface it here
-      Object o = consumingTask.get();
-      assertEquals(producedOrders, actual);
+      Awaitility
+        .await()
+        .atMost(5, TimeUnit.MINUTES)
+        .pollInterval(10, TimeUnit.MILLISECONDS)
+        .until(() -> consumingTask.isDone());
+
+      assertEquals(producedOrders, consumingTask.get());
+
     } catch (ExecutionException ex) {
       fail(ex.getMessage());
+    }
+    finally {
+      if ( ! consumingTask.isDone() )
+        consumingTask.cancel(true);
+      service.awaitTermination(100, TimeUnit.MILLISECONDS);
     }
   }
 }
