446a95015ba862dab57ce21e073595723c780e3d
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2021-02-08 13:17:30 -0600

    DEVX-2500: remove curl output which is leaking into program behavior

--- images/connect-operator/hooks/operator.sh
+++ images/connect-operator/hooks/operator.sh
@@ -112,7 +112,7 @@ function apply_connector() {
       # todo: better handling of errors to assist debugging
 			echo "Updating existing connector config: $connector_name on $BASE_URL"
       echo "$desired_connector_config" > "$connector_name.json"
-    	curl -s -S -XPUT -H "Content-Type: application/json" --data "$desired_connector_config" $curl_user_opt "$BASE_URL/connectors/$connector_name/config" || {
+    	curl -o /dev/null -s -S -XPUT -H "Content-Type: application/json" --data "$desired_connector_config" $curl_user_opt "$BASE_URL/connectors/$connector_name/config" || {
         echo "Error updating exisiting connector config: $connector_name. See "
       }
 		fi
@@ -121,8 +121,7 @@ function apply_connector() {
 
     echo "creating new connector: $connector_name on $BASE_URL"
     echo "$desired_connector_config" > "$connector_name.json"
-    curl -s -S -XPOST -H "Content-Type: application/json" --data "$desired_connector_config" $curl_user_opt "$BASE_URL/connectors"
-
+    curl -o /dev/null -s -S -XPOST -H "Content-Type: application/json" --data "$desired_connector_config" $curl_user_opt "$BASE_URL/connectors"
   }
 }
 
